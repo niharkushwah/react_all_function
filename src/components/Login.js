64d930e1fd9efb5bbcc5e35c9bcb0dfdc5fd7672
Nihar Kushwah <59207039+niharkushwah@users.noreply.github.com>
2024-07-01 18:32:50 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";                  
+import React, { useState } from "react";
 import AuthService from "../auth/auth.service";             
 import { Form, Button, Container, Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
